224b0ba4b9a563f536abbb9153796bf7ffc24460
Manoj-Routhu <78267609+Manoj-Routhu@users.noreply.github.com>
2023-09-13 10:51:42 +0530

    Initial commit

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
-from flask import Flask, render_template, Response
+from flask import Flask, render_template, request, redirect, url_for, Response
 import cv2
 import mediapipe as mp
+import os
+import base64
+import numpy as np  # Import NumPy
 
 app = Flask(__name__)
 
@@ -8,49 +11,51 @@ app = Flask(__name__)
 mp_drawing = mp.solutions.drawing_utils
 mp_pose = mp.solutions.pose
 
-# Initialize the video capture
-cap = cv2.VideoCapture(0)  # Change to the appropriate camera index if necessary
-
-def generate_frames():
+# Function to estimate pose from an uploaded image
+def estimate_pose_from_image(image):
     with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as pose:
-        while True:
-            # Read frame from the camera
-            success, image = cap.read()
-            if not success:
-                break
+        # Convert the image to RGB
+        image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
-            # Convert the image to RGB
-            image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        # Process the image with MediaPipe BlazePose
+        results = pose.process(image_rgb)
 
-            # Process the image with MediaPipe BlazePose
-            results = pose.process(image_rgb)
+        # Draw landmarks on the image
+        if results.pose_landmarks:
+            mp_drawing.draw_landmarks(
+                image,
+                results.pose_landmarks,
+                mp_pose.POSE_CONNECTIONS,
+                mp_drawing.DrawingSpec(color=(0, 255, 0), thickness=2, circle_radius=2),
+                mp_drawing.DrawingSpec(color=(0, 0, 255), thickness=2),
+            )
 
-            # Draw landmarks on the image
-            image_output = image.copy()
-            if results.pose_landmarks:
-                mp_drawing.draw_landmarks(
-                    image_output,
-                    results.pose_landmarks,
-                    mp_pose.POSE_CONNECTIONS,
-                    mp_drawing.DrawingSpec(color=(0, 255, 0), thickness=2, circle_radius=2),
-                    mp_drawing.DrawingSpec(color=(0, 0, 255), thickness=2),
-                )
+        # Encode the image as JPEG
+        ret, buffer = cv2.imencode('.jpg', image)
+        frame = buffer.tobytes()
 
-            # Encode the image as JPEG
-            ret, buffer = cv2.imencode('.jpg', image_output)
-            frame = buffer.tobytes()
-
-            # Yield the frame as a response
-            yield (b'--frame\r\n'
-                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+        # Return the processed frame
+        return frame
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', result=None)
 
-@app.route('/video_feed')
-def video_feed():
-    return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    if 'photo' in request.files:
+        photo = request.files['photo']
+        if photo.filename != '':
+            # Read the uploaded image
+            image = cv2.imdecode(np.fromstring(photo.read(), np.uint8), cv2.IMREAD_COLOR)
+            
+            # Estimate pose and display the result
+            result_frame = estimate_pose_from_image(image)
+            result = base64.b64encode(result_frame).decode("utf-8")
+            
+            return render_template('index.html', result=result)
+    
+    return redirect(url_for('index'))
 
 if __name__ == '__main__':
     app.run(debug=True)
--- static/script.js
+++ static/script.js
@@ -1,50 +1,30 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const video = document.getElementById('videoElement');
-    const canvas = document.getElementById('canvasElement');
-    const context = canvas.getContext('2d');
+    const inputElement = document.getElementById('fileInput');
     const poseImage = document.getElementById('poseImage');
 
-    navigator.mediaDevices.getUserMedia({ video: true })
-        .then((stream) => {
-            video.srcObject = stream;
-            video.onloadedmetadata = () => {
-                // Adjust canvas size to match video dimensions
-                canvas.width = video.videoWidth;
-                canvas.height = video.videoHeight;
+    inputElement.addEventListener('change', (e) => {
+        const file = e.target.files[0];
 
-                video.play();
-                animatePoseEstimation();
-            };
-        })
-        .catch((error) => {
-            console.error('Error accessing the camera:', error);
-        });
+        if (file) {
+            // Create a FormData object to send the file
+            const formData = new FormData();
+            formData.append('photo', file);
 
-    function animatePoseEstimation() {
-        // Draw the current video frame on the canvas
-        context.drawImage(video, 0, 0, canvas.width, canvas.height);
-
-        // Convert canvas image to base64 data
-        const frameData = canvas.toDataURL('image/jpeg');
-
-        // Make an AJAX POST request to the server to send frame data and get pose data
-        fetch('/process', {
-                method: 'POST',
-                body: frameData
-            })
-            .then(response => response.json())
-            .then(data => {
-                // Handle the received pose data
-                const annotatedImage = data.annotated_image;
-                // Display the received pose image in the <img> element
-                poseImage.src = 'data:image/jpeg;base64,' + annotatedImage;
-            })
-            .catch(error => {
-                console.error('Error fetching pose data:', error);
-            })
-            .finally(() => {
-                // Request next frame animation
-                requestAnimationFrame(animatePoseEstimation);
-            });
-    }
+            // Make an AJAX POST request to the server to send the file
+            fetch('/upload', {
+                    method: 'POST',
+                    body: formData
+                })
+                .then(response => response.json())
+                .then(data => {
+                    // Handle the received pose data
+                    const resultImage = data.result;
+                    // Display the received pose image in the <img> element
+                    poseImage.src = 'data:image/jpeg;base64,' + resultImage;
+                })
+                .catch(error => {
+                    console.error('Error uploading file:', error);
+                });
+        }
+    });
 });
